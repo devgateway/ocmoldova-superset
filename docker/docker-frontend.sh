2903da1a914a0b170bc317a02d83a0631e2f59e5
Mihai Postelnicu <mpostelnicu@developmentgateway.org>
2023-08-31 17:46:30 +0300

    more memory for compiling frontend

--- docker/docker-frontend.sh
+++ docker/docker-frontend.sh
@@ -23,7 +23,7 @@ apt install -y chromium
 
 cd /app/superset-frontend
 npm install -f --no-optional --global webpack webpack-cli
-npm install -f --no-optional
+export NODE_OPTIONS=--max_old_space_size=8192 && npm install -f --no-optional
 
 echo "Running frontend"
 npm run dev
